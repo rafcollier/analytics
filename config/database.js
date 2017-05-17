60310dad6592094bbaea4e8d5510241090f7b59b
rafcollier <your_email@domain.com>
2017-05-17 10:00:54 -0400

    tweak

--- config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	database: 'mongodb://localhost:27017/analyticsDB',
-	//database: 'mongodb://heroku_zrw6q2jc:ompdea0o2sd5f2c7al0tbcpcnv@ds133670.mlab.com:33670/heroku_zrw6q2jc',
-	secret: 'theAnalyticsSecret'
-}
-
-//dbuser: heroku_zrw6q2jc
-//dbpassword: ompdea0o2sd5f2c7al0tbcpcnv
-
